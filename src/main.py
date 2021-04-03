21235e7a69701b85f7f2a4fb3e3bd0f14ebfbc6a
Annie-24 <anayin1@hotmail.com>
2021-04-03 19:16:18 +0000

    Created classes and register endpoint

--- src/main.py
+++ src/main.py
@@ -8,13 +8,17 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_current_user, get_jwt_identity
 from models import db, User
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+app.config['JWT_SECRET_KEY'] = 'super-secret'
+#aquí cambiamos a super-secret
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+jwt = JWTManager(app) #se agrega para evitar este error 'You must initialize a JWTManager with this flask application before using this method
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -30,15 +34,37 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
-
+#para obtener todos los usuarios
+@app.route('/users', methods=['GET'])
+def handle_user():
+    users = User.query.all()
+    response_body = []
+    for user in users:
+        response_body.append(user.serialize())
     return jsonify(response_body), 200
 
+#para crear un usuario
+@app.route('/users', methods=['POST'])
+def add_new_professional():
+    body = request.get_json()
+    print (body)
+    new_user = User(
+        full_name = body ["full_name"],
+        password = body ["password"],
+        email = body ["email"],
+        phone = body ["phone"],
+        location = body ["location"],
+       
+    )
+    db.session.add(new_user)
+    try:
+        db.session.commit()
+        print (new_user.serialize())
+        response = {'jwt': create_access_token (identity=new_user.email), "user": new_user.serialize()}
+        return jsonify (response), 201
+    except Exception as error:
+        print (error.args)
+        return jsonify ("NOT CREATE USER"), 500
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
--- src/models.py
+++ src/models.py
@@ -1,19 +1,61 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from base64 import b64encode
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import *
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(120), nullable=False)
+    hashed_password = db.Column(db.String(120), nullable=False)
+    salt = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    phone = db.Column(db.String(25), unique=True, nullable=False)
+    location = db.Column(db.String (50), nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    # def __repr__(self):
+    #     return '<User %r>' % self.username
+
+    
+    def __init__(self,**body):
+        #se crea la función constructora para las instancias
+        self.full_name = body ['full_name']
+        self.salt = b64encode(os.urandom(4)).decode('utf-8')
+        self.hashed_password = self.set_password(body['password'])
+        self.email = body ['email']
+        self.phone = body ['phone']
+        self.location = body ['location']
+       
+        #función para guardar la contraseña
+    def set_password(self, password):
+        return generate_password_hash(
+            f"{password}{self.salt}"
+    ) 
+
+        #función para chequear la contraseña
+    def check_password(self, password):
+        return check_password_hash(
+            self.hashed_password,
+            f"{password}{self.salt}"
+        )
+
+    @classmethod
+    def create (cls,**kwargs):
+        new_user=cls(kwargs)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user    
 
     def serialize(self):
         return {
             "id": self.id,
+            "full_name": self.full_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+            "phone": self.phone,
+            "location": self.location,
+        }    
+
+    
